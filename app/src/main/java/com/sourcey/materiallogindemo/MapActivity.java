f27b8ba85efea59a8b2e4ac2b00a929da8e104c0
ehddgks0627 <ehdgks0627@naver.com>
2017-02-14 20:25:51 +0900

    add1

--- app/src/main/java/com/sourcey/materiallogindemo/MapActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/MapActivity.java
@@ -4,50 +4,18 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
-import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-//
-//
-//public class MapActivity extendsc  Activity implements OnMapReadyCallback {
-//
-//    GpsInfo gi = new GpsInfo();
-//    final LatLng location = new LatLng(gi.getLongitude(), gi.getLatitude());
-//    private GoogleMap googleMap;
-//5
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        MapFragment mapFragment = (MapFragment) getFragmentManager()
-//                .findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(MapActivity.this);
-//    }
-//
-//
-//    @Override
-//    public void onMapReady(final GoogleMap map) {
-//        googleMap = map;
-//
-//        Marker seoul = googleMap.addMarker(new MarkerOptions().position(location)
-//                .title("Seoul"));
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
-//        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
-//    }
-//
-//}
+import com.sourcey.materiallogindemo.GpsInfo;
 
 public class MapActivity extends Activity implements OnMapReadyCallback {
-
+    GpsInfo gi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,24 +25,18 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        gi = new GpsInfo(this);
+        gi.getLocation();
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
-        LatLng sydney = new LatLng(-33.867, 151.206);
+        LatLng sydney = new LatLng(gi.getLatitude(), gi.getLongitude());
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 70));
     }
 
 }
